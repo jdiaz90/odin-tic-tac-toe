df70822c73c167a3e461ced02e488acbecc6e5eb
Javier Díaz <jdr_1_2_1990@hotmail.com>
2023-02-16 17:21:40 +0100

    Refactor functions

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const boardFactory = (player1, player2) => {
 
+    const boardHTML = document.querySelector('.container')
     const board = [
         ["", "", ""],
-        ["", "x", ""],
-        ["", "", "o"],
+        ["", "X", ""],
+        ["", "", "O"],
     ]
 
-    const getCell = (row, col) => document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
+    const fillCell = (row, col) => {
+        const cell = document.createElement('td')
+        cell.classList.add('cell')
+        cell.setAttribute('data-row', row)
+        cell.setAttribute('data-col', col)
+        cell.textContent = board[row][col]
+        if (cell.textContent == '') {
+            cell.addEventListener('click', () => {
+                cell.textContent = 'X'
+            })
+        } 
+        boardHTML.appendChild(cell)
+    }
+
+    const clearBoard = () => {
+
+        while (boardHTML.firstChild) boardHTML.removeChild(boardHTML.firstChild)
+    }
 
     const fillBoard = () => {
         board.forEach((row, indexRow) => {
             row.forEach((col, indexCol) => {
-                const cell = getCell(indexRow, indexCol)
-                cell.textContent = col
+                fillCell(indexRow, indexCol)
             })
         });
     }
 
-    return { board, fillBoard }
+    return { board, fillBoard, clearBoard }
 
 }
 
@@ -30,4 +47,5 @@ const playerFactory = (name, write) => {
 const board = boardFactory(
     playerFactory('Javier', 'X'),
     playerFactory('Lucas', '0'))
+board.clearBoard()
 board.fillBoard()
